8c564f15a0d8ed571e7131752de22d0f97714c0e
DESKTOP-J4ARFF4\KyoungJinChoi <imjaichoipro@gmail.com>
2026-01-23 18:19:14 +0900

    basicSetting 올림

---
+++ components/hello-wave.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
+
+export function HelloWave() {
+  const rotation = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${rotation.value}deg` }],
+  }));
+
+  useEffect(() => {
+    rotation.value = withRepeat(
+      withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
+      4, // repeats 4 times
+      true // reverses the animation
+    );
+  }, []);
+
+  return (
+    <Animated.Text
+      style={[
+        {
+          fontSize: 28,
+          lineHeight: 32,
+          marginTop: -6,
+        },
+        animatedStyle,
+      ]}>
+      👋
+    </Animated.Text>
+  );
+}
